a1886ed22c48a737c1e7efc88e39b3d8e69695bd
Marc Sednaoui <marcsednaoui@hotmail.com>
2021-03-06 18:44:06 +0100

    remove them

--- pages/about.js
+++ pages/about.js
@@ -29,7 +29,7 @@ export default function About() {
                 I ask questions about how humans learn from their environment, question the role of our current education system in the shaping of our method of thinking, how science and technology are progressing and how communities are formed.
             </p>
             <p>
-                The problem I think about the most is how to <b>unlock hidden human potential</b>. Talent is everywhere, opportunity is not. I want to provide the exposure needed for adolescents to find their “spark”. While nature (genes) is a factor in forming our character, our genes and our environment interact. We are ever evolving creatures, even during our lifetime. I want to take a bet on the unknowns, the ones that our society failed them. I want to find the ones that are curious enough to develop their passions, but not fortunate enough with the resources. I want to find the lost minds.
+                The problem I think about the most is how to <b>unlock hidden human potential</b>. Talent is everywhere, opportunity is not. I want to provide the exposure needed for adolescents to find their “spark”. While nature (genes) is a factor in forming our character, our genes and our environment interact. We are ever evolving creatures, even during our lifetime. I want to take a bet on the unknowns, the ones that our society failed. I want to find the ones that are curious enough to develop their passions, but not fortunate enough with the resources. I want to find the lost minds.
             </p>
             <p>
                 I am obsessed with technological innovation. I get excited about crypto, defi, emerging carbon removal technologies, machine learning, brain machine interface, gen tech, and privacy focused applications.
